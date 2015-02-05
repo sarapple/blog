7d69bdfd4c1a2b26b4f9bbbe1305c30ee8d37175
Sara Wong <swong2@wellesley.edu>
2015-02-04 16:59:21 -0800

    new comment model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203210458) do
+ActiveRecord::Schema.define(version: 20150205005409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
